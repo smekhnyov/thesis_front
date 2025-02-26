016be13b524720bf0a30742c2fe4ca9a4896f2ef
smekhnyov <156292095+smekhnyov@users.noreply.github.com>
2025-02-26 16:23:57 +0300

    I forgot to add httpS) (know, i'm stupid)

--- src/api.js
+++ src/api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 export const fetchTables = async () => {
-  const response = await axios.get('http://smekhnyov.ru/table');
+  const response = await axios.get('https://smekhnyov.ru/table');
   return response.data;
 };
 
 export const fetchColumns = async (table) => {
-  const response = await axios.get(`http://smekhnyov.ru/column/${table}`);
+  const response = await axios.get(`https://smekhnyov.ru/column/${table}`);
   return response.data;
 };
 
 export const fetchData = async (table, column) => {
-  const response = await axios.get(`http://smekhnyov.ru/data/${table}/${column}`);
+  const response = await axios.get(`https://smekhnyov.ru/data/${table}/${column}`);
   return response.data;
 };
